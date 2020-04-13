cb24c1e06a515d42df54f558255ce4acd86949a0
Clara Bjuvensjö <clara.bjuvensjo@gmail.com>
2020-04-13 17:33:56 +0200

    added class InputForm

---
+++ src/InputForm.java
@@ -0,0 +1,34 @@
+import javafx.scene.control.Alert;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+public class InputForm extends Alert {
+    private TextField nameField = new TextField();
+    private TextField jewelField = new TextField();
+    private CheckBox materialBox = new CheckBox("Av guld");
+
+    public InputForm() {
+        super(AlertType.CONFIRMATION);
+        GridPane pane = new GridPane();
+        pane.addRow(0, new Label("Namn: ", nameField));
+        pane.addRow(1, new Label("Stenar: ", jewelField));
+        pane.addRow(2, materialBox);
+        getDialogPane().setContent(pane);
+        setHeaderText("Nytt smycke");
+    }
+
+    public String getName(){
+        return nameField.getText();
+    }
+
+    public int getJewels(){
+        return Integer.parseInt(jewelField.getText());
+    }
+
+    public boolean getMaterial(){
+        return materialBox.isSelected();
+    }
+
+}
